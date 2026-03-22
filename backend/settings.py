2a177cf1b7208be2d76228c193a140eb55c6a6ca
SanaPasado <cscunanan@student.hau.edu.ph>
2026-03-22 11:42:36 +0800

    removed anymail

--- backend/settings.py
+++ backend/settings.py
@@ -245,10 +245,11 @@ SIMPLE_JWT = {
     "CHECK_USER_IS_ACTIVE": True,
 }
 
-EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
-ANYMAIL = {
-    "RESEND_API_KEY": os.getenv("RESEND_API_KEY"),
-}
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
+# ANYMAIL config is not needed for console backend, but can be left for future use
+# ANYMAIL = {
+#     "RESEND_API_KEY": os.getenv("RESEND_API_KEY"),
+# }
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "your@email.com")
 PASSWORD_RESET_FRONTEND_URL = os.getenv("PASSWORD_RESET_FRONTEND_URL", "http://localhost:3000/reset-password")
 EMAIL_VERIFICATION_FRONTEND_URL = os.getenv(
